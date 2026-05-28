118afed5491ebbaba18a810f550a13b14aea1ec1
SteamTurbine Developer <steamturbine@xciel.com>
2026-05-28 03:45:11 -0700

    WIP: Pressured pipe texture/model fixes - empty block model, new 16x16 textures, partial models for TESR

--- src/main/java/com/xciel/steamturbine/client/STPartialModels.java
+++ src/main/java/com/xciel/steamturbine/client/STPartialModels.java
@@ -6,9 +6,8 @@ import net.minecraft.resources.ResourceLocation;
 import static com.xciel.steamturbine.SteamTurbine.rl;
 
 public class STPartialModels {
-    public static final PartialModel PIPE_CORE = partial("block/pipe_core");
-    public static final PartialModel PIPE_SPOUT = partial("block/pipe_spout");
-    public static final PartialModel PIPE_HUB = partial("block/pipe_hub");
+    public static final PartialModel PIPE_CORE = partial("block/pressured-pipe-core-straight");
+    public static final PartialModel PIPE_SPOUT = partial("block/pressured-pipe-spout-connection");
 
     private static PartialModel partial(String path) {
         return PartialModel.of(rl(path));
