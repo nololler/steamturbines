317540a5b93f1885cf01f71d69554d39093ad6e0
Xciel <eligahnoah22@gmail.com>
2026-06-05 10:37:53 -0700

    Fix steam turbine hitbox to updated model

--- src/main/java/com/xciel/steamturbine/content/turbine/SteamTurbineBlock.java
+++ src/main/java/com/xciel/steamturbine/content/turbine/SteamTurbineBlock.java
@@ -21,47 +21,43 @@ public class SteamTurbineBlock extends Block implements IBE<SteamTurbineBlockEnt
     public static final Property<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
 
     private static final VoxelShape SHAPE_NORTH = Shapes.or(
-        Block.box(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
         Block.box(5.0, 5.0, 0.0, 11.0, 11.0, 2.0),
         Block.box(5.0, 5.0, 14.0, 11.0, 11.0, 16.0),
-        Block.box(4.0, 3.0, 1.0, 12.0, 12.0, 15.0),
-        Block.box(1.0, 3.0, 8.0, 15.0, 15.0, 10.0),
-        Block.box(2.0, 3.0, 6.0, 14.0, 14.0, 8.0),
-        Block.box(0.0, 3.0, 11.0, 16.0, 16.0, 14.0),
-        Block.box(0.0, 3.0, 2.0, 16.0, 16.0, 5.0)
+        Block.box(4.0, 4.0, 1.0, 12.0, 12.0, 15.0),
+        Block.box(1.0, 1.0, 8.0, 15.0, 15.0, 10.0),
+        Block.box(2.0, 2.0, 6.0, 14.0, 14.0, 8.0),
+        Block.box(0.0, 0.0, 11.0, 16.0, 16.0, 14.0),
+        Block.box(0.0, 0.0, 2.0, 16.0, 16.0, 5.0)
     );
 
     private static final VoxelShape SHAPE_EAST = Shapes.or(
-        Block.box(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
         Block.box(14.0, 5.0, 5.0, 16.0, 11.0, 11.0),
         Block.box(0.0, 5.0, 5.0, 2.0, 11.0, 11.0),
-        Block.box(1.0, 3.0, 4.0, 15.0, 12.0, 12.0),
-        Block.box(6.0, 3.0, 1.0, 8.0, 15.0, 15.0),
-        Block.box(8.0, 3.0, 2.0, 10.0, 14.0, 14.0),
-        Block.box(2.0, 3.0, 0.0, 5.0, 16.0, 16.0),
-        Block.box(11.0, 3.0, 0.0, 14.0, 16.0, 16.0)
+        Block.box(1.0, 4.0, 4.0, 15.0, 12.0, 12.0),
+        Block.box(6.0, 1.0, 1.0, 8.0, 15.0, 15.0),
+        Block.box(8.0, 2.0, 2.0, 10.0, 14.0, 14.0),
+        Block.box(2.0, 0.0, 0.0, 5.0, 16.0, 16.0),
+        Block.box(11.0, 0.0, 0.0, 14.0, 16.0, 16.0)
     );
 
     private static final VoxelShape SHAPE_SOUTH = Shapes.or(
-        Block.box(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
         Block.box(5.0, 5.0, 14.0, 11.0, 11.0, 16.0),
         Block.box(5.0, 5.0, 0.0, 11.0, 11.0, 2.0),
-        Block.box(4.0, 3.0, 1.0, 12.0, 12.0, 15.0),
-        Block.box(1.0, 3.0, 6.0, 15.0, 15.0, 8.0),
-        Block.box(2.0, 3.0, 8.0, 14.0, 14.0, 10.0),
-        Block.box(0.0, 3.0, 2.0, 16.0, 16.0, 5.0),
-        Block.box(0.0, 3.0, 11.0, 16.0, 16.0, 14.0)
+        Block.box(4.0, 4.0, 1.0, 12.0, 12.0, 15.0),
+        Block.box(1.0, 1.0, 6.0, 15.0, 15.0, 8.0),
+        Block.box(2.0, 2.0, 8.0, 14.0, 14.0, 10.0),
+        Block.box(0.0, 0.0, 2.0, 16.0, 16.0, 5.0),
+        Block.box(0.0, 0.0, 11.0, 16.0, 16.0, 14.0)
     );
 
     private static final VoxelShape SHAPE_WEST = Shapes.or(
-        Block.box(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
         Block.box(0.0, 5.0, 5.0, 2.0, 11.0, 11.0),
         Block.box(14.0, 5.0, 5.0, 16.0, 11.0, 11.0),
-        Block.box(1.0, 3.0, 4.0, 15.0, 12.0, 12.0),
-        Block.box(8.0, 3.0, 1.0, 10.0, 15.0, 15.0),
-        Block.box(6.0, 3.0, 2.0, 8.0, 14.0, 14.0),
-        Block.box(11.0, 3.0, 0.0, 14.0, 16.0, 16.0),
-        Block.box(2.0, 3.0, 0.0, 5.0, 16.0, 16.0)
+        Block.box(1.0, 4.0, 4.0, 15.0, 12.0, 12.0),
+        Block.box(8.0, 1.0, 1.0, 10.0, 15.0, 15.0),
+        Block.box(6.0, 2.0, 2.0, 8.0, 14.0, 14.0),
+        Block.box(11.0, 0.0, 0.0, 14.0, 16.0, 16.0),
+        Block.box(2.0, 0.0, 0.0, 5.0, 16.0, 16.0)
     );
 
     public SteamTurbineBlock(Properties properties) {
